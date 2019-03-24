fc6f62285d7543c15ab3880f9f4fe414ae766300
Didar Korkembay <didar490@gmail.com>
2019-03-23 18:31:32 -0700

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Didar Korkembay on 3/23/19.
+//  Copyright © 2019 Didar Korkembay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
